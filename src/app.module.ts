2b32a3382e1289eb17f348d332306a0fa59abdee
Mohammad Omar <mohamedsiddig5026485@gmail.com>
2022-03-11 02:20:23 -0500

    starting point!
    this is my starting point for this practice project :(

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static'; // New
+import { join } from 'path'; // New
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client/dist'),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
