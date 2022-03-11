15d9b2b38239822bf1330742361e27e5c146607e
Mohammad Omar <mohamedsiddig5026485@gmail.com>
2022-03-11 07:22:11 -0500

    Sucessfully configured the database
    database was successfully connected to

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ServeStaticModule } from '@nestjs/serve-static'; // New
-import { join } from 'path'; // New
+import { ServeStaticModule } from '@nestjs/serve-static'; // New for VueJS client
+import { join } from 'path'; // New for VueJS client
+
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'client/dist'),
+      ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..', '../client/dist'),
     }),
+    TypeOrmModule.forRoot(),
   ],
   controllers: [AppController],
   providers: [AppService],
