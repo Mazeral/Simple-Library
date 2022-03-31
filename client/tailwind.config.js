d54748b950907fca6c462201b2dc9a4ec54b6c5c
Mohammad Omar <mohamedsiddig5026485@gmail.com>
2022-03-31 11:44:28 +0200

    Configured tailwind
    Successfully configured tailwing and the intellisense is working.

---
+++ client/tailwind.config.js
@@ -0,0 +1,10 @@
+/* eslint-env node */
+module.exports = {
+  mode: "jit",
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
