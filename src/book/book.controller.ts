7256f7851eb1468a6444f7c81b0de4be084a5b6d
Mohammad Omar <mohamedsiddig5026485@gmail.com>
2022-05-13 14:37:11 +0200

    Deleted versioning
    deleted versioning because I have no clue how to use it properly

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -14,23 +14,19 @@ import { CreateBookDto } from './dto/create-book.dto';
 @Controller('book')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
-  @Version('0')
   @Post()
   create(@Body() createBookDto: CreateBookDto) {
     return this.bookService.create(createBookDto);
   }
-  @Version('0')
   @Get()
   findAll() {
     return this.bookService.findAll();
   }
-  @Version('0')
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.bookService.findOne(+id);
   }
 
-  @Version('0')
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -39,7 +35,6 @@ export class BookController {
     return this.bookService.update(+id, updateBook);
   }
 
-  @Version('0')
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.bookService.remove(+id);
