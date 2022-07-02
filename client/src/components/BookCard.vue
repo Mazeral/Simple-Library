3d20368c6b76de9a49152c0a87463fd2d5dc463f
Mohammad Omar <mohamedsiddig5026485@gmail.com>
2022-07-02 17:57:12 +0200

    Fixed fetch error

--- client/src/components/BookCard.vue
+++ client/src/components/BookCard.vue
@@ -1,20 +1,24 @@
 <script setup>
 import { ref } from 'vue';
-let books = ref(null);
-books =  fetch('localhost:3000/api/book/')
+let books = ref([]);
+fetch('http://localhost:3000/api/book/', {
+  method: 'GET',
+  mode: 'cors',
+})
   .then((res) => res.json())
   .then((data) => (books.value = data))
-  .catch((err) => console.log(err));
-
-console.log(books);
+  .catch((err) => console.log(err.message));
+console.log(books.value);
 </script>
 
 <template>
-  <div class="card" v-for="book in books" :key="book.id">
-    <div class="card-header">{{ book.title }}</div>
-    <div class="card-body">{{ book.description }}</div>
+  <div class="container">
+    <div class="card" v-for="book in books" :key="book.id">
+      <div class="card-header">{{ book.title }}</div>
+      <div class="card-body">{{ book.description }}</div>
+    </div>
+    <div>{{ books.value }}</div>
   </div>
-  <div>{{ books.value }}</div>
 </template>
 
 <style></style>
