56b594dcfd88820bab4bcff1648b8aef797f2460
Mohammad Omar <mohamedsiddig5026485@gmail.com>
2022-09-28 20:49:54 +0200

    rails

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-const port = process.env.PORT || 3001;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors();
-  await app.listen(port);
+  await app.listen(process.env.PORT, '0.0.0.0');
 }
 bootstrap();
