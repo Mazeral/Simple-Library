8a4b41c536e1d12236bf5487c6829c6ea33539fd
Mohammad Omar <mohamedsiddig5026485@gmail.com>
2022-09-28 20:30:47 +0200

    render

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+const port = process.env.PORT || 3001;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors();
-  await app.listen(process.env.PORT, '0.0.0.0');
+  await app.listen(port);
 }
 bootstrap();
