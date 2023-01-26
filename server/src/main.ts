4dd6f28ae24bed5689efa296edb0d7efefbad1ac
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-01-26 14:53:22 +0200

    Railway edits.

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(`0.0.0.0:$PORT`);
+  await app.listen(`${PORT}`);
 }
 bootstrap();
